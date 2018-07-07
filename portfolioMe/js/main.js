98d4f743c60600344842e1d61e3616c5cc74569a
catdog990 <ozmcastro@gmail.com>
2018-07-07 05:32:38 -0400

    trying to add featureDiscovery

--- portfolioMe/js/main.js
+++ portfolioMe/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
   
-
-
-
   $('.carousel').carousel();
+
+  $('.tap-target').featureDiscovery();
+  $('.tap-target').featureDiscovery('open');
+
   });
+  function closeFeatureDiscovery() {
+    $('.tap-target').featureDiscovery('close');
+  }
