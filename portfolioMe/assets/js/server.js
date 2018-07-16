3b748aab71fe6cb2d8b756deab1d5ae0ecad0255
catdog990 <ozmcastro@gmail.com>
2018-07-15 21:34:13 -0400

    able to deploy to heroku now, hopefully

--- portfolioMe/assets/js/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-var app = express();
-var PORT = 3000;
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
---
+++ portfolioMe/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const realRoutes = require('./routes');
+
+const app = express();
+ app.set('port', ( process.env.PORT || 3000));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(realRoutes);
+
+
+app.listen(app.get('port'), () => {
+  console.log('App running on port', app.get('port'));
+
+});
+
